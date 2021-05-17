f9a2d75cb871438993da73a8062f86a2b6baf4be
Taina Conde <tainabatistaconde@gmail.com>
2021-05-17 17:47:49 -0400

    feat: Create Links folder in Navbar
    Links contains the components related to the navigation.
    Move StyledNavLinks to Links.

--- src/components/App/Navbar/StyledNav.js
+++ src/components/App/Navbar/StyledNav.js
@@ -1,10 +1,14 @@
 import styled from 'styled-components';
 
 const StyledNav = styled.nav`
+display: flex;
+justify-content: space-between;
+align-items: baseline;
 height: 114px;
 background: #000000 0% 0% no-repeat padding-box;
 box-shadow: 0px 3px 6px #FF000087;
 opacity: 1;
+padding: 0 50px;
 `
 
 export default StyledNav;
