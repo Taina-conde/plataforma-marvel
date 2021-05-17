64e36fb2130c63052d4d6b4065208ae7621b24c1
Taina Conde <tainabatistaconde@gmail.com>
2021-05-17 15:52:01 -0400

    feat: StyledNavLink - create component and render it in Navbar
    Take the css from design. Next I will add fonts to assets.

--- src/components/App/Navbar/index.js
+++ src/components/App/Navbar/index.js
@@ -1,8 +1,17 @@
 import StyledNav from './StyledNav';
+import StyledNavLink from './StyledNavLink';
 const Navbar = () => {
     return (
         <StyledNav>
-            
+            <StyledNavLink to = '/'>
+                Personagens
+            </StyledNavLink>
+            <StyledNavLink to = '/filmes'>
+                Filmes
+            </StyledNavLink>
+            <StyledNavLink to = '/hqs'>
+                HQs
+            </StyledNavLink>
         </StyledNav>
     )
 } 
