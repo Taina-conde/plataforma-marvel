5b4e191a8b581535eb307281ea9f58d46444655c
Taina Conde <tainabatistaconde@gmail.com>
2021-05-17 19:23:30 -0400

    feat: Export background image from designs to img folder in assets

---
+++ src/components/App/PersonagensView/index.js
@@ -0,0 +1,9 @@
+import Wrapper from '../../shared/Wrapper/'
+const PersonagensView = () => {
+    return (
+        <Wrapper>
+            
+        </Wrapper>
+    )
+}
+export default PersonagensView;
--- src/components/App/index.js
+++ src/components/App/index.js
@@ -5,6 +5,7 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 function App() {
   return (
     <Router>
+      
       <Navbar/>
     </Router>
     
