1bfa47d1a25580ef9ef9df5bd561bf7583a39757
Taina Conde <tainabatistaconde@gmail.com>
2021-05-17 16:50:52 -0400

    style: Remove layout styles from StyledNav

--- src/components/App/Navbar/StyledNav.js
+++ src/components/App/Navbar/StyledNav.js
@@ -1,9 +1,6 @@
 import styled from 'styled-components';
 
 const StyledNav = styled.nav`
-top: -4px;
-left: -17px;
-width: 1398px;
 height: 114px;
 background: #000000 0% 0% no-repeat padding-box;
 box-shadow: 0px 3px 6px #FF000087;
--- src/components/App/Navbar/StyledNavLink.js
+++ src/components/App/Navbar/StyledNavLink.js
@@ -2,11 +2,7 @@ import styled from "styled-components";
 import { NavLink } from "react-router-dom";
 
 const StyledNavLink = styled(NavLink)`
-  top: 50px;
-  left: 809px;
-  width: 108px;
-  height: 30px;
-  text-align: left;
+  font-size: 30px;
   font: normal normal bold 30px/36px Axiforma;
   letter-spacing: 0px;
   color: #ffffff;
