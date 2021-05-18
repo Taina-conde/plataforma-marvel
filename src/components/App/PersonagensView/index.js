2404550a2367661521c546ad1860b515a50ecf45
Taina Conde <tainabatistaconde@gmail.com>
2021-05-18 16:24:50 -0400

    feat: Make Wrapper a reausible component for all the views
    The wrapper will contain the cards and render
    as many cards as there is items in each
    view content.

--- src/components/App/PersonagensView/index.js
+++ src/components/App/PersonagensView/index.js
@@ -1,10 +1,11 @@
+import { useSelector } from 'react-redux';
 import Wrapper from '../../shared/Wrapper/';
 import Card from '../../shared/Card/'
 const PersonagensView = () => {
+    const personagens = useSelector(state => state.personagens);
+    const personagensArr = Object.keys(personagens);
     return (
-        <Wrapper>
-            <Card>PERSONAGENS</Card>
-        </Wrapper>
+        <Wrapper content = {personagensArr}/>
     )
 }
 export default PersonagensView;
--- src/components/shared/Wrapper/index.js
+++ src/components/shared/Wrapper/index.js
@@ -1,6 +1,14 @@
-import StyledWrapper from './StyledWrapper';
+import Card from "../Card";
+import StyledWrapper from "./StyledWrapper";
 
-const Wrapper = props => {
-    return <StyledWrapper>{props.children}</StyledWrapper>;
-}
+const Wrapper = (props) => {
+  const { content } = props;
+  return (
+    <StyledWrapper>
+      {content.map((item) => {
+        return <Card key = {item.name}>{item.name}</Card>;
+      })}
+    </StyledWrapper>
+  );
+};
 export default Wrapper;
