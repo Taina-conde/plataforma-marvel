c14adec15d222385745e5d5169b317c95f28dccc
Taina Conde <tainabatistaconde@gmail.com>
2021-05-18 10:28:00 -0400

    feat: Create MenuButton component
    To make the menu responsive, create MenuButton component
    that will appear for small devices.

---
+++ src/components/App/Navbar/MenuButton/index.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+const MenuButton = styled.button`
+    border: 1 px solid #ffffff;
+    color: #ffffff;
+    opacity: 0.25;
+    padding: 15px;
+    border-radius: 10px;
+`
+export default MenuButton;
