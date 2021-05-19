583f83d95f8cc5093891d34795e536354973a54c
Taina Conde <tainabatistaconde@gmail.com>
2021-05-19 15:46:53 -0400

    refactor: Remove unused folders and files in DetailsCard

--- src/components/shared/Card/DetailsCard/HqsDetails/index.js
+++
@@ -1,6 +0,0 @@
-import {useSelector} from "react-redux";
-const HqsDetails = () => {
-    const card = useSelector( state => state.selectedCard.card)
-    return 
-}
-export default HqsDetails;
--- src/components/shared/Card/DetailsCard/PersonagensDetails/index.js
+++
@@ -1,6 +0,0 @@
-import {useSelector} from "react-redux";
-const PersonagensDetails = () => {
-    const card = useSelector( state => state.selectedCard.card)
-    return 
-}
-export default PersonagensDetails;
